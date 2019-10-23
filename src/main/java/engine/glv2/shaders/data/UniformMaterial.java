07669fff98f23c72533e34309b3df28347cf7b9e
Guerra24 <pablo230699@hotmail.com>
2019-10-22 23:14:57 -0600

    Modernized UniformMaterial, removed UniformArray

--- src/main/java/engine/glv2/shaders/data/UniformMaterial.java
+++ src/main/java/engine/glv2/shaders/data/UniformMaterial.java
@@ -1,59 +1,43 @@
-/*
- * This file is part of Light Engine
- * 
- * Copyright (C) 2016-2019 Lux Vacuos
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * 
- */
-
 package engine.glv2.shaders.data;
-
-import static org.lwjgl.opengl.GL20C.glUniform1f;
-import static org.lwjgl.opengl.GL20C.glUniform1i;
-import static org.lwjgl.opengl.GL20C.glUniform3f;
 
 import engine.gl.MaterialGL;
 
-public class UniformMaterial extends UniformArray {
+public class UniformMaterial extends UniformObject {
 
-	private MaterialGL currentValue;
+	private UniformSampler diffuseTex;
+	private UniformSampler normalTex;
+	private UniformSampler roughnessTex;
+	private UniformSampler metallicTex;
+	private UniformFloat roughness;
+	private UniformFloat metallic;
+	private UniformFloat reflective;
+	private UniformVec3 diffuse;
+	private UniformVec3 emissive;
 
-	public UniformMaterial(String matName) {
-		super(matName + ".diffuseTex", matName + ".normalTex", matName + ".metallicTex", matName + ".roughnessTex",
-				matName + ".metallic", matName + ".roughness", matName + ".reflective", matName + ".diffuse",
-				matName + ".emissive");
+	public UniformMaterial(String name) {
+		diffuseTex = new UniformSampler(name + ".diffuseTex");
+		normalTex = new UniformSampler(name + ".normalTex");
+		metallicTex = new UniformSampler(name + ".metallicTex");
+		roughnessTex = new UniformSampler(name + ".roughnessTex");
+		roughness = new UniformFloat(name + ".roughness");
+		metallic = new UniformFloat(name + ".metallic");
+		reflective = new UniformFloat(name + ".reflective");
+		diffuse = new UniformVec3(name + ".diffuse");
+		emissive = new UniformVec3(name + ".emissive");
+		super.storeUniforms(diffuseTex, normalTex, metallicTex, roughnessTex, roughness, metallic, reflective, diffuse,
+				emissive);
 	}
 
-	public void loadMaterial(MaterialGL value) {
-		if (!used || !currentValue.equals(value)) {
-
-			glUniform1i(super.getLocation()[0], 0);
-			glUniform1i(super.getLocation()[1], 1);
-			glUniform1i(super.getLocation()[2], 2);
-			glUniform1i(super.getLocation()[3], 3);
-
-			glUniform1f(super.getLocation()[4], value.getMetalness());
-			glUniform1f(super.getLocation()[5], value.getRoughness());
-			glUniform1f(super.getLocation()[6], value.getReflective());
-			glUniform3f(super.getLocation()[7], value.getColor().x(), value.getColor().y(), value.getColor().z());
-			glUniform3f(super.getLocation()[8], value.getEmissive().x(), value.getEmissive().y(),
-					value.getEmissive().z());
-
-			used = true;
-			currentValue = value;
-		}
+	public void loadMaterial(MaterialGL material) {
+		diffuseTex.loadTexUnit(0);
+		normalTex.loadTexUnit(1);
+		metallicTex.loadTexUnit(2);
+		roughnessTex.loadTexUnit(3);
+		roughness.loadFloat(material.getRoughness());
+		metallic.loadFloat(material.getMetalness());
+		reflective.loadFloat(material.getReflective());
+		diffuse.loadVec3(material.getColor());
+		emissive.loadVec3(material.getEmissive());
 	}
 
 }
