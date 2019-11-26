bae224dd9c8f2add06dabc57f006167868971b61
orange451 <drew.hamilton@mac.com>
2019-11-25 21:12:03 -0500

    IDE now uses lwjgui callbacks

--- src/main/java/ide/IDE.java
+++ src/main/java/ide/IDE.java
@@ -46,10 +46,9 @@ public class IDE extends RenderableApplication {
 		Theme.setTheme(new ThemeDark());
 		
 		// Redraw window if resized
-		GLFW.glfwSetWindowSizeCallback(window, new GLFWWindowSizeCallbackI() {
+		win.addEventListener(new lwjgui.event.listener.WindowSizeListener() {
 			@Override
-			public void invoke(long handle, int wid, int hei) {
-				LWJGUI.render();
+			public void invoke(long arg0, int arg1, int arg2) {
 				renderThread.forceUpdate();
 			}
 		});
