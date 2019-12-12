82aa0f36265fe13955eafa1883d44d6f9dfb6e3a
orange451 <drew.hamilton@mac.com>
2019-12-11 22:31:28 -0500

    Improved player physics object

--- src/main/java/engine/lua/type/object/insts/PlayerPhysics.java
+++ src/main/java/engine/lua/type/object/insts/PlayerPhysics.java
@@ -107,7 +107,7 @@ public class PlayerPhysics extends PhysicsBase implements TreeViewable {
 		if ( !this.isOnGround() )
 			stepHeight /= 4f;
 		
-		if ( this.getVelocity().getZ() > -0.3 && this.getVelocity().getZ() <= 0 )
+		if ( this.physics.getVelocity().z > -0.3 && this.physics.getVelocity().z <= 0 )
 			extraStepHeight = stepHeight;
 		
 		PhysicsWorld physics = Game.workspace().getPhysicsWorld();
@@ -117,7 +117,7 @@ public class PlayerPhysics extends PhysicsBase implements TreeViewable {
 		int rings = 4;
 	
 		// Calculate distance between rings
-		float margin = 0.01f;
+		float margin = 0.001f;
 		float radius = this.getRadius();
 		float march = (float) ((radius - margin) / rings); 
 	
@@ -130,14 +130,15 @@ public class PlayerPhysics extends PhysicsBase implements TreeViewable {
 		// Cast all rays
 		ClosestRayResultCallback[] callbacks = new ClosestRayResultCallback[steps * rings +1];
 		callbacks[callbacks.length - 1] = physics.rayTestClosest( new Vector3f( x, y, z ), new Vector3f( 0, 0, -(stepHeight+extraStepHeight) ));
-		for (int ii = 0; ii < rings; ii++) {
+		for (int ii = 1; ii <= rings; ii++) {
 			for (int i = 0; i < steps; i++) {
 				float frac = (float) ((i/(float)steps) * Math.PI * 2);
-				float size = (radius - margin) - (march * ii);
+				float size = (march * ii) - margin;
+				//size *= 2;
 				float xx = (float) (x + (Math.cos(frac + ii) * size));
 				float yy = (float) (y + (Math.sin(frac + ii) * size));
 	
-				callbacks[(ii * steps) + i] = physics.rayTestClosest( new Vector3f( xx, yy, z ), new Vector3f( 0, 0, -(stepHeight+extraStepHeight) ));
+				callbacks[((ii-1) * steps) + i] = physics.rayTestClosest( new Vector3f( xx, yy, z ), new Vector3f( 0, 0, -(stepHeight+extraStepHeight) ));
 			}
 		}
 		
@@ -156,16 +157,16 @@ public class PlayerPhysics extends PhysicsBase implements TreeViewable {
 			float d = z - hitPoint.z;
 			if (d < dist) {
 				// Perform another ray test to see if there is any geometry right above the potential floor.
-				ClosestRayResultCallback temp = physics.rayTestClosest( new Vector3f( hitPoint.x, hitPoint.y, hitPoint.z + (radius * 1.1f) ), new Vector3f( 0, 0, getHeight()-radius ));
+				ClosestRayResultCallback temp = physics.rayTestClosest( new Vector3f( hitPoint.x, hitPoint.y, z ), new Vector3f( 0, 0, getHeight()-radius ));
 				Vector3 tempHitPoint = new Vector3();
 				temp.getHitPointWorld(tempHitPoint);
 				float dz = tempHitPoint.z - hitPoint.z;
 	
 				// If there is nothing blocking the ray, then it is a potential position we can move into!
-				//if ( temp.getCollisionObject() == null || dz >= getHeight()-radius ) {
+				if ( temp.getCollisionObject() == null || dz >= getHeight()-radius || temp.getCollisionObject() == this.physics.getBody() ) {
 					closest = callback;
 					dist = d;
-				//}
+				}
 			}
 		}
 		
@@ -189,181 +190,6 @@ public class PlayerPhysics extends PhysicsBase implements TreeViewable {
 			
 			// Mark that we're on ground
 			onGround = true;
-		}
-		
-		this.forceset(C_ONGROUND, LuaValue.valueOf(onGround));
-	}
-	
-	private void doFloorCollision() {
-		float stepHeight = this.getStepHeight();
-		if ( !this.isOnGround() )
-			stepHeight /= 4f;
-		PhysicsWorld physics = Game.workspace().getPhysicsWorld();
-	
-		// 64 rays cast
-		int steps = 16;
-		int rings = 4;
-	
-		// Calculate distance between rings
-		float margin = 0.01f;
-		float radius = this.getRadius();
-		float march = (float) ((radius - margin) / rings);
-	
-		// get starting location
-		Vector3f origin = this.physics.getLocation().sub(0,0,this.getHeight()/2f);
-		float x = origin.x;
-		float y = origin.y;
-		float z = origin.z + stepHeight;
-	
-		// Cast all rays
-		ClosestRayResultCallback[] callbacks = new ClosestRayResultCallback[steps * rings +1];
-		callbacks[callbacks.length - 1] = physics.rayTestClosest( new Vector3f( x, y, z ), new Vector3f( 0, 0, -stepHeight ));
-		for (int ii = 0; ii < rings; ii++) {
-			for (int i = 0; i < steps; i++) {
-				float frac = (float) ((i/(float)steps) * Math.PI * 2);
-				float size = (radius - margin) - (march * ii);
-				float xx = (float) (x + (Math.cos(frac + ii) * size));
-				float yy = (float) (y + (Math.sin(frac + ii) * size));
-	
-				callbacks[(ii * steps) + i] = physics.rayTestClosest( new Vector3f( xx, yy, z ), new Vector3f( 0, 0, -stepHeight ));
-			}
-		}
-	
-		Vector3f slope = new Vector3f(0, 0, 1);
-		int amtSteep = 0;
-		int amtFlat = 0;
-		float steepAngle = 0.82f;
-	
-		// Loop through all rays and find one closest to the ground
-		float dist = 999;
-		ClosestRayResultCallback lowest = null;
-		for (int i = 0; i < callbacks.length; i++) {
-			ClosestRayResultCallback callback = callbacks[i];
-	
-			// Ignore the ray if it's above you (bug)
-			/*if ( callback.getHitPointWorld(new Vector3());.z > z )
-				continue;*/
-			
-			Vector3 hitPoint = new Vector3();
-			callback.getHitPointWorld(hitPoint);
-	
-			// find lowest ray
-			float d = z - hitPoint.z;
-			if (d < dist) {
-				// Perform another ray test to see if there is any geometry right above the potential floor.
-				ClosestRayResultCallback temp = physics.rayTestClosest( new Vector3f( hitPoint.x, hitPoint.y, hitPoint.z + 0.5f ), new Vector3f( 0, 0, 1 ));
-				Vector3 tempHitPoint = new Vector3();
-				temp.getHitPointWorld(tempHitPoint);
-				float dz = tempHitPoint.z - hitPoint.z;
-	
-				// If there is nothing blocking the ray, then it is a potential position we can move into!
-				if ( tempHitPoint.z == 0 || dz >= getHeight() ) {
-					lowest = callback;
-					dist = d;
-				}
-			}
-	
-			// Find slope
-			/*if (d < stepHeight) {
-				Vector3 hitNormal = new Vector3();
-				callback.getHitNormalWorld(hitNormal);
-				
-				//hitNormal.normalize();
-				if (Math.abs(hitNormal.z) <= steepAngle) {
-					slope.add(hitNormal.x, hitNormal.y, hitNormal.z);
-					amtSteep++;
-				}else{
-					amtFlat++;
-				}
-			}*/
-		}
-	
-		// Put distace at very bottom of collision shape.
-		dist -= stepHeight;
-	
-		// Get current velocity;
-		Vector3f newVel = this.getVelocity().toJoml();
-	
-		// Don't look at floor collisions if velocity is going up
-		if (newVel.z > 4.0)
-			return;
-	
-		// Reset motion states
-		boolean wasOnGroundBefore = this.isOnGround();
-		PhysicsObjectInternal objectHit = null;
-		Vector3f hitVelocity = this.getVelocity().toJoml();
-		boolean onGround = false;
-		boolean sliding = false;
-		if ( lowest != null && dist < stepHeight ) {
-			onGround = true;
-			Vector3 lowestHitPoint = new Vector3();
-			lowest.getHitPointWorld(lowestHitPoint);
-			
-			float toz = lowestHitPoint.z;
-			float zDelta = toz - origin.z;
-	
-			// If we're moving upwards
-			if ( zDelta > 0.6 ) {
-				// Do not climb up objects that are too steep OR are a moving rigid body
-				if ( Math.abs( slope.z ) < steepAngle || Math.abs( slope.z ) > 1.0 ) {
-					toz = origin.z;
-				}
-			}
-	
-			// Calculate new position to stand on
-			Vector3f toPos = new Vector3f(x, y, toz + getHeight()/2f);
-			if ( lowest.getCollisionObject() != null ) {
-				objectHit = physics.getPhysicsObject((btRigidBody) lowest.getCollisionObject());
-			}
-	
-			// Move the player along with moving objects that he's standing on
-			/*if (lowest.getCollisionObject() != null) {
-				Vector3f vel = lowest.collisionObject.getInterpolationLinearVelocity(new Vector3f());
-				toPos.x += vel.x * GameEngine.delta;
-				toPos.y += vel.y * GameEngine.delta;
-				toPos.z += vel.z * GameEngine.delta;
-				objectHit = physics.findObject( (RigidBody) lowest.collisionObject );
-				if (objectHit != null) {
-					
-					// Set the material we're on to the material directly under us
-					materialOn = objectHit.getSoundMaterial();
-					
-					// hacky stuff dont look
-					Vector3f start = lowest.rayFromWorld;
-					Vector3f start1 = new Vector3f( start.x, start.y, start.z );
-					ClosestRayResultCallback callmebackpls = physics.rayTestClosestWorld(start1, new Vector3f( 0, 0, -1 ), false );
-					if ( callmebackpls.collisionObject != null ) {
-						PhysicsObject tempObj = physics.findObject( (RigidBody) callmebackpls.collisionObject );
-						if ( tempObj != null) {
-							materialOn = tempObj.getSoundMaterial();
-						}
-					}
-				}
-			}*/
-	
-			// Teleport player to the correct location
-			this.physics.teleport( toPos );
-	
-			// Check for a hill
-			if (amtSteep > amtFlat) {
-				slope.normalize();
-				newVel.add(slope.mul( newVel.dot(slope) * -1f ));
-				//newVel = VectorMath.add(VectorMath.mult(slope, VectorMath.dot(newVel, slope) * -1f), newVel);
-				sliding = true;
-				onGround = false;
-			} else {
-				newVel.z = 0;
-			}
-	
-			// Set the z velocity. 0 if on ground.
-			this.physics.setVelocity(newVel);
-		}
-		
-		// If we hit the ground
-		if ( !wasOnGroundBefore && onGround && objectHit != null && objectHit.getBody().getInvMass() > 0 ) {
-			Vector3f nv = new Vector3f( hitVelocity.x / 2f, hitVelocity.y / 2f, hitVelocity.z / 2f );
-			Vector3f v = objectHit.getVelocity().add(nv);
-			objectHit.setVelocity( v );
 		}
 		
 		this.forceset(C_ONGROUND, LuaValue.valueOf(onGround));
