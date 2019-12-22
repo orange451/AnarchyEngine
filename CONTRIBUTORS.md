e36aaa8eed0171b82d662b9a7e0791d26ea78fac
Guerra24 <pablo230699@hotmail.com>
2019-12-21 20:55:21 -0600

    Re-license as MPL 2.0

---
+++ CONTRIBUTORS.md
@@ -0,0 +1,4 @@
+The following is a list of contributors to the [Anarchy Engine](https://github.com/orange451/AnarchyEngine) project, in alphabetical order:
+
+ * [Andrew Hamilton](https://github.com/orange451)
+ * [Pablo González](https://github.com/Guerra24) <<guerra24@luxvacuos.net>>
--- src/main/resources/assets/shaders/Particle.fs
+++ src/main/resources/assets/shaders/Particle.fs
@@ -1,22 +1,12 @@
-//
-// This file is part of Light Engine
-//
-// Copyright (C) 2016-2019 Lux Vacuos
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-//
-//
+﻿/*
+ *
+ * Copyright (C) 2015-2020 Anarchy Engine Open Source Contributors (see CONTRIBUTORS.md)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
 
 in vec2 textureCoords0;
 in vec2 textureCoords1;
