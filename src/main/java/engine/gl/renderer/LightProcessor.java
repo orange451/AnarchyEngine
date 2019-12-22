b33f47de4fe80ecd608616fe9f25725d37f27220
Guerra24 <pablo230699@hotmail.com>
2019-12-21 22:19:00 -0600

    Added license header to files

--- src/main/java/engine/gl/renderer/LightProcessor.java
+++ src/main/java/engine/gl/renderer/LightProcessor.java
@@ -1,3 +1,14 @@
+/*
+ *
+ * Copyright (C) 2015-2020 Anarchy Engine Open Source Contributors (see CONTRIBUTORS.md)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
+
+
 package engine.gl.renderer;
 
 import engine.gl.LegacyPipeline;
--- src/main/java/engine/lua/type/object/insts/Connection.java
+++ src/main/java/engine/lua/type/object/insts/Connection.java
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright (C) 2015-2020 Anarchy Engine Open Source Contributors (see CONTRIBUTORS.md)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
+
 package engine.lua.type.object.insts;
 
 import java.util.concurrent.atomic.AtomicLong;
