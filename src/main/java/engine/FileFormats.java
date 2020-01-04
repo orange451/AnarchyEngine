b306566280469e425d19c50fbe1d99e840b40035
orange451 <drew.hamilton@mac.com>
2020-01-04 14:17:59 -0500

    Added copyright to FileFormats

--- src/main/java/engine/FileFormats.java
+++ src/main/java/engine/FileFormats.java
@@ -1,3 +1,13 @@
+/*
+ *
+ * Copyright (C) 2015-2020 Anarchy Engine Open Source Contributors (see CONTRIBUTORS.md)
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ *
+ */
+
 package engine;
 
 public class FileFormats {
@@ -8,4 +18,6 @@ public class FileFormats {
 	public static final String TEXTURES = "png,bmp,tga,jpg,hdr";
 
 	public static final String PREFABS = MESHES + ",md5anim";
+	
+	public static final String ANIMATIONS = "smd,md5anim";
 }
